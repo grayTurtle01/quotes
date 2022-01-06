20eb73de4815b9fd943423a0ff7d72cde6f5fbc7
grayTurtle01 <the.correo@protonmail.com>
2022-01-06 16:23:02 -0600

    change color with each new quote

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-let quotes = [ { quote: 'Cogito Ergo Sum',
+let quotes = [ 
+                    { quote: 'Cogito Ergo Sum',
                        author: 'Descartes'},
+
                     { quote: 'El hombre es el lobo del hombre',
                       author: 'Hobbes'
                       },
@@ -23,15 +25,38 @@ function get_random_quote(){
   return random_quote
 }
 
+function get_random_color(){
+  let colors = ['lightgray', 'ligthblue', 'gray', 'salmon', 'peru', 'lightpink']
+
+  let index = Math.floor(Math.random()*colors.length)
+  let color = colors[index] 
+
+  return color
+}
+
+function change_color(color) {
+  let body = document.querySelector('body')
+  body.style.background = color
+
+  document.querySelector('#new-quote').style.background = color
+  document.querySelector('#tweet-quote').style.background = color
+
+}
+
 function change_quote(){
   random_quote = get_random_quote()
   
   document.querySelector("#text").innerText = random_quote.quote
   document.querySelector("#author").innerText = '-' + random_quote.author
+
+  color = get_random_color()
+  change_color(color)
 }
               
 function initial_state(){
   change_quote()
 }
+
+
 
 initial_state()
